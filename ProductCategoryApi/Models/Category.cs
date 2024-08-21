4a0983ec74647ffe8a63021a89e107fbfc13745b
AHMED, Rakib <Rakib.AHMED@econocom.com>
2024-08-21 20:29:42 +0200

    Created necessary files for Model

---
+++ ProductCategoryApi/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductCategoryApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<ProductCategoryApi> ProductCategories { get; set; }
+
+    }
+}
