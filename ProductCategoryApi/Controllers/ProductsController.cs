c98131c3c4f2efd9d842409554bd404838b8679d
AHMED, Rakib <Rakib.AHMED@econocom.com>
2024-08-25 14:21:34 +0200

    Added prodocut controller

---
+++ ProductCategoryApi/Controllers/ProductsController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductCategoryApi.Data;
+using ProductCategoryApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductCategoryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductCategoryContext _context;
+
+
+
+        public ProductsController(ProductCategoryContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/products
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+        }
+
+        // GET: api/products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.Where(p => !p.IsDeleted).ToListAsync();
+        }
+
+        // GET: api/products/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null || product.IsDeleted)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/products/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingProduct = await _context.Products.FindAsync(id);
+            if (existingProduct == null || existingProduct.IsDeleted)
+            {
+                return NotFound();
+            }
+
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.Price = product.Price;
+            existingProduct.IsDeleted = product.IsDeleted;
+
+            _context.Entry(existingProduct).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/products/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> SoftDeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null || product.IsDeleted)
+            {
+                return NotFound();
+            }
+
+            product.IsDeleted = true;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
